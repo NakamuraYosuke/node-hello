7ea91e0f8cd69ba2cbfadd120e3a53767e15a197
NakamuraYosuke <nakanakau@gmail.com>
2020-02-09 21:54:41 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var express    = requre('express'),
+    app        = express(),
+    bodyParser = requre('body-parser'),
+    os         = require('os'),
+    hostname   = os.hostname();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+var route = express.Router();
+
+app.user('/', route);
+
+route.get('/', function(req, res) {
+    res.send('Hi! I am running on host -> ' + hostname + '\n');
+});
+
+route.get('/health', function(req, res) {
+    res.send('OK\n');
+});
+
+app.listen(port, ip);
+console.log('nodejs server running on http://%s:%s', ip, port);
+
+module.exports = app;
+
